e3ee37ab2c1896043b89184b888a45a75ffd5d82
Rilr20 <nrrl99@gmail.com>
2025-05-08 19:13:22 +0200

    stopped using position absolute

--- frontend/src/components/Display.js
+++ frontend/src/components/Display.js
@@ -1,23 +1,77 @@
-// frontend/src/components/Display.jsx
+import React from 'react'
 
-import React from 'react';
+export default function Display({ squares, width, height }) {
+    try {
+        let sortedSquares = [...squares].sort((a, b) => {
+            const aY = parseInt(a.square.split(",")[1])
+            const bY = parseInt(b.square.split(",")[1])
 
-const Display = ({ width, height, colour, pos }) => {
+            return aY - bY;
+        })
+
+        const rows = [];
+
+        sortedSquares.forEach(({ square, color }, index) => {
+            const [x, y] = square.split(",").map(Number);
+            console.log(index);
+
+            if (!rows[y]) {
+                console.log(rows[y]);
+                rows[y] = []
+
+            }
+            rows[y].push({ x, y, color })
+        })
+        function addBuffers(firstRow, lastRow, squares) {
+            console.log(lastRow);
+
+            let y = lastRow[0].y;
+            let x = Math.min(...lastRow.map(s => s.x));
+            console.log(x);
+            console.log(y);
+
+            if (firstRow.length !== lastRow.length && x - 1 >= 0) {
+                let idx = 1;
+                while (lastRow.length !== firstRow.length) {
+                    lastRow.unshift({ x: x - idx, y: y, color: "" })
+                    idx++
+                }
+            }
+        }
+        addBuffers(rows[0], rows[rows.length - 1])
+
+        const sortedRowKeys = Object.keys(rows).map(Number).sort((a, b) => a - b);
+        return <div style={{ display: "flex", flexDirection: "column",}}>
+            {sortedRowKeys.map((y) => (
+                <div style={{ display: "flex",  }}>
+                    {rows[y]
+                        .sort((a, b) => a.x - b.x)
+                        .map(({ x, color }, i) => (
+                            <div
+                                style={{
+                                    width: "40px",
+                                    height: "40px",
+                                    margin: "5px",
+                                    backgroundColor: color,
+                                    border: color != "" ? "1px solid black" : "1px solid white"
+                                }}
+                            ></div>
+                        ))}
+                </div>
+            ))}
+        </div>
+
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+
+// const Display = ({ squares, width, height }) => {
+
     
-    let split = pos.split(",")
-    let hexColour = colour.replace("#", '')
-    const inverted = (0xFFFFFF - parseInt(hexColour, 16)).toString(16).padStart(6, '0');
-    const styles = {
-        width: `${width}px`,
-        height: `${height}px`,
-        backgroundColor: colour,
-        position: 'absolute',
-        left: `${(parseInt(split[0]) * width) + 15*split[0]}px`,
-        top: `${(parseInt(split[1]) * height) + 15* split[1]}px`,
-        border: `3px solid #${inverted}`
-    };
 
-    return <div style={styles}></div>;
-};
+// };
 
-export default Display;
+// export default Display;
